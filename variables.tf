537aaf8961de6aabd5e3759e49ac8629432cf771
tasinight <betta.investments@gmail.com>
2022-06-01 19:56:15 -0400

    terraform module

---
+++ variables.tf
@@ -0,0 +1,28 @@
+variable "region" {
+  default = "us-east-2"
+}
+
+variable "accesskey" {
+	default="" 
+}
+
+variable "secretkey" {
+	default=""
+}
+
+variable "vpcID" {
+	default="vpc-24b9d54f"
+}
+variable "amiID" {
+	default="ami-0ba62214afa52bec7"
+}
+variable "keyPair" {
+	default="myohiokeys"
+}
+variable "keyPair1" {
+	default="myohiokeys.pem"
+}
+
+
+
+
